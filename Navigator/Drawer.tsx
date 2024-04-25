879dcdf08bf46125d7c32465d0235410423d6796
Simon Navarrete <s.navarretecabrera@uandresbello.edu>
2024-04-25 17:38:55 -0400

    Create initial Screens

--- Navigator/Drawer.tsx
+++ Navigator/Drawer.tsx
@@ -1,14 +1,17 @@
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import React from 'react'
-import { Welcome } from '../Views/Welcome';
 import { NavigationContainer } from '@react-navigation/native';
+import { ToDoList } from '../Views/ToDoList';
+import { DeletedList } from '../Views/DeletedList';
 
 export const Drawer = () => {
     const Drawer = createDrawerNavigator();
     return (
         <NavigationContainer>
             <Drawer.Navigator>
-                <Drawer.Screen name="List" component={Welcome} />
+                <Drawer.Screen name="List" component={ToDoList} />
+                <Drawer.Screen name="Deleted items" component={DeletedList} />
+
             </Drawer.Navigator>
         </NavigationContainer>
     )
---
+++ Views/ToDoList.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+export const ToDoList = () => {
+    return (
+        <View>
+            <Text>
+                To Do List
+            </Text>
+        </View>
+    )
+}
